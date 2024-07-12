a920f85ed79036604658e7d43f55cff591e2d015
Rajeshd01 <rajeshd01042004@gmail.com>
2024-07-12 10:53:34 +0530

    Added input controls and serach functionalities

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,19 +1,19 @@
-import { FaTrashAlt } from 'react-icons/fa';
-import LineItem from './LineItem';
+import { FaTrashAlt } from "react-icons/fa";
+import LineItem from "./LineItem";
 
-const itemList = ({items, handleCheck, handleDelete}) => {
+const itemList = ({ items, handleCheck, handleDelete }) => {
   return (
     <ul>
-                    {items.map((item) => (
-                        <LineItem
-                            key={item.id}
-                            item= {item}
-                            handleCheck = {handleCheck}
-                            handleDelete = {handleDelete}
-                        />
-                    ))}
-                </ul>
-  )
-}
+      {items.map((item) => (
+        <LineItem
+          key={item.id}
+          item={item}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ))}
+    </ul>
+  );
+};
 
-export default itemList
+export default itemList;
